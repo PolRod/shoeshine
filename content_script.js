25e76f4ac223a5fd68254e1f2114729379f3c818
PolRod <pol@lumoslabs.com>
2017-07-29 19:11:44 -0700

    Transition to price slide

--- content_script.js
+++ content_script.js
@@ -4,14 +4,33 @@ chrome.runtime.sendMessage({html: "initial"}, function(response) {
   var html = response.html;
   $('body').prepend(html);
   $('#shoeshine-overlay').css('z-index', 1000);
-  $('#shoeshine-hello').css('z-index', 1001);
+  $('.shoeshine-slide').each(function(){
+    $(this).css('z-index', 1001);
+  });
 });
+
+function activateSlide(slideElement){
+  $(slideElement).addClass('shoeshine-active');
+  $(slideElement).css('top', '140%');
+  $(slideElement).show();
+  $(slideElement).animate({top: '40%'}, 100);
+  $(slideElement).find('input').focus();
+}
+
+function deActivateSlide(slideElement){
+  $(slideElement).removeClass('shoeshine-active');
+  $(slideElement).animate({top: '-140%'}, 100);
+  $(slideElement).hide();
+}
 
 $(document).ready(function(){
   $('.shoeshine-active input').focus();
+  // Going from hello page to price page
   $('.shoeshine-active input').keypress(function(e) {
+    //If key pressed is enter key
     if (e.which == 13) {
-      console.log( "Handler for .keypress() called." );
+      deActivateSlide($('#shoeshine-hello'));
+      activateSlide($('#shoeshine-price'));
     }
   });
 });
