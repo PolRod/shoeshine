d43187c52d775e8aeec11d962f35d8ada5a2cbfa
Pol Rodriguez <8934233+PolRod@users.noreply.github.com>
2017-08-12 19:51:27 -0700

    Change continue button value

--- content_script.js
+++ content_script.js
@@ -29,12 +29,15 @@ function switchActiveSlide(deActivateElement, activateElement){
 }
 
 $(document).ready(function(){
+  var intendedPurchase = "";
+  var intendedPurchasePrice = "";
   $('body').addClass('shoeshine-stop');
   $('.shoeshine-active input').focus();
   // Going from hello page to price page
   $('#shoeshine-hello input').keypress(function(e) {
     // If key pressed is enter key
     if (e.which == 13) {
+      intendedPurchase = this.value;
       switchActiveSlide($('#shoeshine-hello'), $('#shoeshine-price'));
     }
   });
@@ -42,6 +45,8 @@ $(document).ready(function(){
   $('#shoeshine-price input').keypress(function(e) {
     // If key pressed is enter key
     if (e.which == 13) {
+      intendedPurchasePrice = this.value;
+      $('.choice-continue .choice-button').text('Buy a ' + intendedPurchase);
       switchActiveSlide($('#shoeshine-price'), $('#shoeshine-choice'));
     }
   });
