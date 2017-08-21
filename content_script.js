dd8767addfe0e5aad7e785a51aaaffdbe7675ea3
Pol Rodriguez <8934233+PolRod@users.noreply.github.com>
2017-08-20 19:00:34 -0700

    Get json from content js

--- content_script.js
+++ content_script.js
@@ -1,3 +1,15 @@
+//get info in costs.json
+var costs = {};
+var costsUrl = chrome.runtime.getURL("resources/costs.json");
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       costs = JSON.parse(xhttp.responseText);
+    }
+};
+xhttp.open("GET", costsUrl, true);
+xhttp.send();
+
 // The actual html is in a background page. Here we ask for the html to that page.
 // We also make some CSS adjustments that were not effective if executed
 // through content_script.css.
