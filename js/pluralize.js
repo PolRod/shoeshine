3a5d804ccf1c0b9cdb6794cf5e03187b5a169eef
Pol Rodriguez <8934233+PolRod@users.noreply.github.com>
2017-12-10 21:14:22 -0800

    Improve pluralizer

--- js/pluralize.js
+++ js/pluralize.js
@@ -1,4 +1,23 @@
+const QUANT_REGEXP = /%quant%\d*/;
+
+function multiplyQuantity(text, quantity) {
+  // Get the possible multiplier for quantity, located after the last % (eg %quant%1000)
+  multiplier = text.match(QUANT_REGEXP)[1];
+  if (!multiplier) { multiplier = 1 };
+  return quantity*multiplier;
+}
+
+function quantifyText(text, quantity) {
+  // Add a space at the end because the regex we are using for
+  // .replace targets the trailing space as well
+  return text.replace(QUANT_REGEXP, quantity);
+}
+
+
 function pluralize(text, quantity) {
+  quantity = multiplyQuantity(text, quantity);
+  text = quantifyText(text, quantity);
+
   return text.split(" ").map(function(e){
     pluralizerIndex = e.indexOf('$');
     if (pluralizerIndex > -1) {
