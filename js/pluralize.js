916b848f18b88fba75291634667d79a57a27f439
Pol Rodriguez <8934233+PolRod@users.noreply.github.com>
2018-01-08 22:42:23 -0800

    Extract pluralizeWord

--- js/pluralize.js
+++ js/pluralize.js
@@ -1,33 +1,36 @@
-const QUANT_REGEXP = /%quant%\d*/;
+const QUANT_REGEXP = /%quant%\d*/g;
+const PLURALIZE_REGEXP = /\S*\$\S*/g;
 
 function multiplyQuantity(text, quantity) {
   // Get the possible multiplier for quantity, located after the last % (eg %quant%1000)
-  multiplier = text.match(QUANT_REGEXP)[1];
+  var multiplier = text.match(QUANT_REGEXP)[1];
   if (!multiplier) { multiplier = 1 };
   return quantity*multiplier;
 }
 
 function quantifyText(text, quantity) {
-  // Add a space at the end because the regex we are using for
-  // .replace targets the trailing space as well
   return text.replace(QUANT_REGEXP, quantity);
 }
 
+function pluralizeWord(text, plural) {
+  var modes = text.split('$');
+  var singular = modes[0];
+  if (modes.length == 1) { return singular }
+  var plural = modes[1];
+  if (plural == "") { return singular + 's' }
+  return plural;
+}
 
 function pluralize(text, quantity) {
-  quantity = multiplyQuantity(text, quantity);
-  text = quantifyText(text, quantity);
+  var quantity = multiplyQuantity(text, quantity);
+  var text = quantifyText(text, quantity);
 
-  return text.split(" ").map(function(e){
-    pluralizerIndex = e.indexOf('$');
-    if (pluralizerIndex > -1) {
-      if (pluralizerIndex == e.length -1) {
-        return quantity > 1 ? e.replace("$", "s") : e.slice(0, -1);
-      } else {
-        return quantity > 1 ? e.split("$")[1] : e.split("$")[0];
-      }
-    } else {
-      return e;
+  var pluralizableWords = text.match(PLURALIZE_REGEXP);
+
+  if (pluralizableWords) {
+    for (var i = 0; i < pluralizableWords.length; i++) {
+      text = text.replace(pluralizableWords[i], pluralizeWord(pluralizableWords[i], quantity > 1))
     }
-  }).join(" ");
+  }
+  return text;
 }
